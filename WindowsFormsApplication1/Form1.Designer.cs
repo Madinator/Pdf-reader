a4a1f663b6fb9b77d1f956b8c6a91311d081b4c1
Madinator <48565715+Madinator@users.noreply.github.com>
2019-04-20 15:57:07 +0500

    Add files via upload

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.axAcroPDF1 = new AxAcroPDFLib.AxAcroPDF();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.convertTXTToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.convertImageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.turnViewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.turnToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.rotateToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.goToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lastPageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.nextPageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.previousPageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fullScreenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.axAcroPDF1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // axAcroPDF1
+            // 
+            this.axAcroPDF1.Enabled = true;
+            this.axAcroPDF1.Location = new System.Drawing.Point(2, 27);
+            this.axAcroPDF1.MinimumSize = new System.Drawing.Size(800, 600);
+            this.axAcroPDF1.Name = "axAcroPDF1";
+            this.axAcroPDF1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axAcroPDF1.OcxState")));
+            this.axAcroPDF1.Size = new System.Drawing.Size(1400, 944);
+            this.axAcroPDF1.TabIndex = 5;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1,
+            this.fileToolStripMenuItem,
+            this.viewToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(784, 24);
+            this.menuStrip1.TabIndex = 8;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(37, 20);
+            this.toolStripMenuItem1.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(127, 22);
+            this.openToolStripMenuItem.Text = "Open PDF";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(127, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.convertTXTToolStripMenuItem,
+            this.convertImageToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.fileToolStripMenuItem.Text = "Convert";
+            // 
+            // convertTXTToolStripMenuItem
+            // 
+            this.convertTXTToolStripMenuItem.Name = "convertTXTToolStripMenuItem";
+            this.convertTXTToolStripMenuItem.Size = new System.Drawing.Size(153, 22);
+            this.convertTXTToolStripMenuItem.Text = "Convert txt";
+            this.convertTXTToolStripMenuItem.Click += new System.EventHandler(this.convertTXTToolStripMenuItem_Click);
+            // 
+            // convertImageToolStripMenuItem
+            // 
+            this.convertImageToolStripMenuItem.Name = "convertImageToolStripMenuItem";
+            this.convertImageToolStripMenuItem.Size = new System.Drawing.Size(153, 22);
+            this.convertImageToolStripMenuItem.Text = "Convert screen";
+            this.convertImageToolStripMenuItem.Click += new System.EventHandler(this.convertImageToolStripMenuItem_Click);
+            // 
+            // viewToolStripMenuItem
+            // 
+            this.viewToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.turnViewToolStripMenuItem,
+            this.goToolStripMenuItem,
+            this.lastPageToolStripMenuItem,
+            this.nextPageToolStripMenuItem,
+            this.previousPageToolStripMenuItem,
+            this.fullScreenToolStripMenuItem});
+            this.viewToolStripMenuItem.Enabled = false;
+            this.viewToolStripMenuItem.Name = "viewToolStripMenuItem";
+            this.viewToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.viewToolStripMenuItem.Text = "View";
+            // 
+            // turnViewToolStripMenuItem
+            // 
+            this.turnViewToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.turnToolStripMenuItem,
+            this.rotateToolStripMenuItem});
+            this.turnViewToolStripMenuItem.Name = "turnViewToolStripMenuItem";
+            this.turnViewToolStripMenuItem.Size = new System.Drawing.Size(148, 22);
+            this.turnViewToolStripMenuItem.Text = "Turn view ";
+            // 
+            // turnToolStripMenuItem
+            // 
+            this.turnToolStripMenuItem.Name = "turnToolStripMenuItem";
+            this.turnToolStripMenuItem.Size = new System.Drawing.Size(194, 22);
+            this.turnToolStripMenuItem.Text = "Turn clockwise";
+            this.turnToolStripMenuItem.ToolTipText = "CTRL + H + PLUS";
+            this.turnToolStripMenuItem.Click += new System.EventHandler(this.turnToolStripMenuItem_Click);
+            // 
+            // rotateToolStripMenuItem
+            // 
+            this.rotateToolStripMenuItem.Name = "rotateToolStripMenuItem";
+            this.rotateToolStripMenuItem.Size = new System.Drawing.Size(194, 22);
+            this.rotateToolStripMenuItem.Text = "Turn counterclockwise";
+            this.rotateToolStripMenuItem.ToolTipText = "CTRL+H+MINUS";
+            this.rotateToolStripMenuItem.Click += new System.EventHandler(this.rotateToolStripMenuItem_Click);
+            // 
+            // goToolStripMenuItem
+            // 
+            this.goToolStripMenuItem.Name = "goToolStripMenuItem";
+            this.goToolStripMenuItem.Size = new System.Drawing.Size(148, 22);
+            this.goToolStripMenuItem.Text = "First page";
+            this.goToolStripMenuItem.Click += new System.EventHandler(this.goToolStripMenuItem_Click);
+            // 
+            // lastPageToolStripMenuItem
+            // 
+            this.lastPageToolStripMenuItem.Name = "lastPageToolStripMenuItem";
+            this.lastPageToolStripMenuItem.Size = new System.Drawing.Size(148, 22);
+            this.lastPageToolStripMenuItem.Text = "Last page";
+            this.lastPageToolStripMenuItem.Click += new System.EventHandler(this.lastPageToolStripMenuItem_Click);
+            // 
+            // nextPageToolStripMenuItem
+            // 
+            this.nextPageToolStripMenuItem.Name = "nextPageToolStripMenuItem";
+            this.nextPageToolStripMenuItem.Size = new System.Drawing.Size(148, 22);
+            this.nextPageToolStripMenuItem.Text = "Next page";
+            this.nextPageToolStripMenuItem.Click += new System.EventHandler(this.nextPageToolStripMenuItem_Click);
+            // 
+            // previousPageToolStripMenuItem
+            // 
+            this.previousPageToolStripMenuItem.Name = "previousPageToolStripMenuItem";
+            this.previousPageToolStripMenuItem.Size = new System.Drawing.Size(148, 22);
+            this.previousPageToolStripMenuItem.Text = "Previous page";
+            this.previousPageToolStripMenuItem.Click += new System.EventHandler(this.previousPageToolStripMenuItem_Click);
+            // 
+            // fullScreenToolStripMenuItem
+            // 
+            this.fullScreenToolStripMenuItem.Name = "fullScreenToolStripMenuItem";
+            this.fullScreenToolStripMenuItem.Size = new System.Drawing.Size(148, 22);
+            this.fullScreenToolStripMenuItem.Text = "Full screen";
+            // 
+            // Form1
+            // 
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnableAllowFocusChange;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.axAcroPDF1);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.MinimumSize = new System.Drawing.Size(800, 600);
+            this.Name = "Form1";
+            this.Text = "Converter";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            ((System.ComponentModel.ISupportInitialize)(this.axAcroPDF1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private AxAcroPDFLib.AxAcroPDF axAcroPDF1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem convertTXTToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem convertImageToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem viewToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem turnViewToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem turnToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem rotateToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem goToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem lastPageToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem nextPageToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem previousPageToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem fullScreenToolStripMenuItem;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Windows.Forms;
+using iTextSharp.text.pdf;
+using iTextSharp.text;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+            size_of_screen();
+        }
+     
+        private void cur_page()
+        {
+            VScrollBar vsb = new VScrollBar();
+            vsb.Scroll += new System.Windows.Forms.ScrollEventHandler(vScroller_Scroll);
+            
+        }
+
+        private void vScroller_Scroll(object sender, ScrollEventArgs e)
+        {
+            MessageBox.Show("123");
+        }
+
+        private void size_of_screen()
+        {
+            int x = SystemInformation.VirtualScreen.Width;
+            int y = SystemInformation.VirtualScreen.Height - axAcroPDF1.Location.Y - 50;
+            
+            axAcroPDF1.Size = new System.Drawing.Size(x, y);
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog odf = new OpenFileDialog();
+            odf.Filter = "Pdf files (*.pdf)|*.pdf";
+
+            /*if(odf.FileName != "")
+            {
+                axAcroPDF1.LoadFile(odf.FileName);
+            }*/
+
+            
+            if (odf.ShowDialog() == DialogResult.OK)
+            {
+
+                axAcroPDF1.src = odf.FileName;
+                viewToolStripMenuItem.Enabled = true;
+                               
+            }
+            
+        }
+
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void convertTXTToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog odf = new OpenFileDialog();
+            odf.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+            string path_to_desktop = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            odf.InitialDirectory = path_to_desktop;
+            if (odf.ShowDialog() == DialogResult.OK)
+            {
+                string path = odf.FileName.ToString();
+
+
+            }
+            try
+            {
+                List<string> data = File.ReadAllLines(odf.FileName.ToString()).ToList();
+                Document doc = new Document();
+                PdfWriter.GetInstance(doc, new FileStream(path_to_desktop + "\\result_txt.pdf", FileMode.Create, FileAccess.Write));
+                doc.Open();
+                foreach (string s in data)
+                {
+                    doc.Add(new Paragraph(s));
+                }
+                doc.Close();
+            }
+            catch (ArgumentException)
+            {
+                
+            }
+            
+        }
+
+        private void convertImageToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Document document = new Document();
+            OpenFileDialog odf = new OpenFileDialog();
+            string path_to_desktop = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+            PdfWriter.GetInstance(document, new FileStream(path_to_desktop + "\\result_jpg.pdf",FileMode.Create));
+            odf.Filter = "Jpg files (*.jpg)|*.jpg";
+            odf.InitialDirectory = path_to_desktop;
+            document.Open();
+
+            if (odf.ShowDialog() == DialogResult.OK)
+            {
+                string path = odf.FileName.ToString();
+                System.Drawing.Image image = System.Drawing.Image.FromFile(path);
+                iTextSharp.text.Image itext= iTextSharp.text.Image.GetInstance(image, System.Drawing.Imaging.ImageFormat.Jpeg);
+                document.Add(itext);
+                document.Close();
+            }
+
+            /*using (var stream = new FileStream("test.pdf", FileMode.Create, FileAccess.Write, FileShare.None))
+            {
+                PdfWriter.GetInstance(document, stream);
+                document.Open();
+                using (var imageStream = new FileStream("test.jpg", FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+                {
+                    var image = Image.GetInstance(imageStream);
+                    document.Add(image);
+                }
+                document.Close();
+            }*/
+        }
+
+        private void turnToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            SendKeys.Send("+^{ADD}");
+            
+        }
+
+        private void rotateToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SendKeys.Send("+^{SUBTRACT}");
+        }
+
+
+
+        private void goToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            axAcroPDF1.gotoFirstPage();
+        }
+
+        private void lastPageToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            axAcroPDF1.gotoLastPage();
+        }
+
+        private void nextPageToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            axAcroPDF1.gotoNextPage();
+        }
+
+        private void previousPageToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            axAcroPDF1.gotoPreviousPage();
+        }
+
+        
+    }
+
+}
+
